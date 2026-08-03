53625667dc601fcc67d400c0405a2100a4821563
Lynn Dergham <lynn@example.com>
2026-08-03 17:25:10 +0300

    add puh

---
+++ rendu/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+
+    char res;
+    while(ac == 2)
+    {
+        if(av[1][i] >= 'a' && av[1][i] <= 'z')
+            res = 'z' - (c - 'a');
+
+        if(av[1][i] >= 'A' && av[1][i] <= 'A')
+            res = 'Z' - (c - 'A');
+
+        write(1,&c,1);
+        i++;
+    }
+    write(1, "\n",1);
+}
