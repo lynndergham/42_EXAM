27a3acc25f531e34259167e506cf021dfd07dae5
Lynn Dergham <lynn@example.com>
2026-08-03 17:38:13 +0300

    add puh

--- rendu/alpha_mirror/alpha_mirror.c
+++ rendu/alpha_mirror/alpha_mirror.c
@@ -5,18 +5,22 @@ int main(int ac, char **av)
     int i = 0;
 
     char res;
-    while(ac == 2)
+    if(ac == 2)
     {
+        while(av[1][i])
+        {
         res = av[1][i];
 
         if(res >= 'a' && res <= 'z')
             res = 'z' - (res - 'a');
 
-        if(res >= 'A' &&res <= 'A')
+        if(res >= 'A' &&res <= 'Z')
             res = 'Z' - (res - 'A');
 
         write(1,&res,1);
-        i++;
+        i++; 
+        }
+        write(1, "\n",1);
     }
-    write(1, "\n",1);
+    
 }
