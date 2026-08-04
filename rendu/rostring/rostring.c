b76b4db9b8ce8ae971704f2f11eb90e8b5caae26
Lynn Dergham <lynn@example.com>
2026-08-04 18:05:37 +0300

    push

--- rendu/rostring/rostring.c
+++ rendu/rostring/rostring.c
@@ -1,56 +1,44 @@
 #include <unistd.h>
 
-int	is_space(char c)
-{
-	return (c == ' ' || c == '\t');
-}
-
-void	print_word(char *s)
-{
-	int	i = 0;
-
-	while (s[i] && !is_space(s[i]))
-		write(1, &s[i++], 1);
-}
-
 int	main(int ac, char **av)
 {
 	int	i = 0;
 	int	start;
 
-	if (ac != 2)
-		return (write(1, "\n", 1), 0);
-
-	while (is_space(av[1][i]))
-		i++;
-	start = i;
-
-	while (av[1][i] && !is_space(av[1][i]))
-		i++;
-
-	if (!av[1][i])
+	if (ac == 2)
 	{
-		print_word(av[1] + start);
-		write(1, "\n", 1);
-		return (0);
-	}
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		start = i;
 
-	while (is_space(av[1][i]))
-		i++;
+		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+			i++;
 
-	while (av[1][i])
-	{
-		if (is_space(av[1][i]))
+		if (av[1][i] == '\0')
 		{
-			write(1, " ", 1);
-			while (is_space(av[1][i]))
-				i++;
+			while (av[1][start] && av[1][start] != ' '
+				&& av[1][start] != '\t')
+				write(1, &av[1][start++], 1);
 		}
 		else
-			write(1, &av[1][i++], 1);
-	}
+		{
+			while (av[1][i] == ' ' || av[1][i] == '\t')
+				i++;
 
-	write(1, " ", 1);
-	print_word(av[1] + start);
+			if (av[1][i])
+			{
+				while (av[1][i])
+				{
+					write(1, &av[1][i], 1);
+					i++;
+				}
+				write(1, " ", 1);
+			}
+
+			while (av[1][start] && av[1][start] != ' '
+				&& av[1][start] != '\t')
+				write(1, &av[1][start++], 1);
+		}
+	}
 	write(1, "\n", 1);
 }
