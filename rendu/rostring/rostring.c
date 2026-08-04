8564da547b16044850f554671cdfae7635fbeebf
Lynn Dergham <lynn@example.com>
2026-08-04 18:25:05 +0300

    push

--- rendu/rostring/rostring.c
+++ rendu/rostring/rostring.c
@@ -1,44 +1,46 @@
+
 #include <unistd.h>
 
-int	main(int ac, char **av)
-{
-	int	i = 0;
-	int	start;
+int print_words(char *s, int start) {
+  int i;
+  int first;
 
-	if (ac == 2)
-	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
-			i++;
-		start = i;
+  i = start;
+  first = 1;
+  while (s[i]) {
+    while (s[i] == ' ' || s[i] == '\t')
+      i++;
+    if (s[i] && !first)
+      write(1, " ", 1);
+    while (s[i] && s[i] != ' ' && s[i] != '\t') {
+      write(1, &s[i], 1);
+      first = 0;
+      i++;
+    }
+  }
+  return (!first);
+}
 
-		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
-			i++;
+void write_word(char *s, int start, int end) {
+  while (start < end)
+    write(1, &s[start++], 1);
+}
 
-		if (av[1][i] == '\0')
-		{
-			while (av[1][start] && av[1][start] != ' '
-				&& av[1][start] != '\t')
-				write(1, &av[1][start++], 1);
-		}
-		else
-		{
-			while (av[1][i] == ' ' || av[1][i] == '\t')
-				i++;
+int main(int argc, char **argv) {
+  int i;
+  int w1s;
 
-			if (av[1][i])
-			{
-				while (av[1][i])
-				{
-					write(1, &av[1][i], 1);
-					i++;
-				}
-				write(1, " ", 1);
-			}
-
-			while (av[1][start] && av[1][start] != ' '
-				&& av[1][start] != '\t')
-				write(1, &av[1][start++], 1);
-		}
-	}
-	write(1, "\n", 1);
+  if (argc >= 2) {
+    i = 0;
+    while (argv[1][i] == ' ' || argv[1][i] == '\t')
+      i++;
+    w1s = i;
+    while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+      i++;
+    if (print_words(argv[1], i))
+      write(1, " ", 1);
+    write_word(argv[1], w1s, i);
+  }
+  write(1, "\n", 1);
+  return (0);
 }
