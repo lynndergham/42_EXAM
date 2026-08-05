9a7ff4b0e02acd59acb301cdf1ddbc4eb7b11c3c
Lynn Dergham <lynn@example.com>
2026-08-05 08:49:57 +0300

    push

--- rendu/alpha_mirror/alpha_mirror.c
+++ rendu/alpha_mirror/alpha_mirror.c
@@ -1,23 +1,22 @@
 #include <unistd.h>
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-    int i = 0;
-    char c;
-
-    if(ac == 2)
+    if(argc == 2)
     {
-        c = av[1][i];
-
-        while(av[1][i])
+        int i = 0;
+        while(argv[1][i])
         {
+            char c;
+            c = argv[1][i];
             if(c >= 'a' && c <= 'z')
-                c = 'z' -(c - 'a');
-            else if(c >= 'A' && c <= 'Z')
-                c = 'z' - (c - 'A');
-            write(1,&c,1);
+                c = 'z' - (c - 'a');
+            else if ( c>= 'A' && c <= 'Z')
+                c = 'Z' - (c - 'A');
+            write(1, &c, 1);
             i++;
-        }
 
+        }
     }
-    write(1,"\n",1);
+    write(1, "\n",1);
+    return (0);
 }
