20dea25e437af52d061b587089e6c7efc2f913ae
Lynn Dergham <lynn@example.com>
2026-08-06 20:48:40 +0300

    push

---
+++ rendu/last_word/last_word.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+int main(int ac, char **av)
+{
+    int i = 0;
+    int end;
+
+    if(ac == 2)
+    {
+        while(av[1][i])
+            i++;
+        i--;
+        
+        while( i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
+            i--;
+        end = i;
+        while(i >= 0 && av[1][i] != ' ' && av[1][i] != '\t')
+            i--;
+        i++;
+        while(i <= end)
+        {
+            write(1,&av[1][i],1);
+            i++;
+        }
+        
+    }
+    write(1,"\n",1);
+}
