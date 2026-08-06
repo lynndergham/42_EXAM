e4061e46f24e4c3f3525c6c2fb5314eee88d19df
Lynn Dergham <lynn@example.com>
2026-08-06 21:37:43 +0300

    push

--- rendu/print_hex/print_hex.c
+++ rendu/print_hex/print_hex.c
@@ -1,5 +1,26 @@
 #include <unistd.h>
 
+int ft_atoi(const char *str)
+{
+    int sign = 1;
+    int result = 0;
+
+    while (*str == ' ' || (*str >= 9 && *str <= 13))
+        str++;
+    if (*str == '-' || *str == '+')
+    {
+        if (*str == '-')
+            sign = -1;
+        str++;
+    }
+    while (*str >= '0' && *str <= '9')
+    {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    return (result * sign);
+}
+
 void print_hex(int n)
 {
     char *hex = "0123456789abcdef";
